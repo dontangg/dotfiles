6993e228ef27773b4ee04a9fa5dbee2185be905a
Don Wilson <robert.don.wilson@gmail.com>
2013-01-12 00:23:19 -0700

    Fix prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -41,30 +41,11 @@ need_push () {
 }
 
 rb_prompt(){
-  if $(( $+commands[rbenv] ))
+  if (( $+commands[rbenv] ))
   then
 	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
 	else
 	  echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(( $+commands[todo.sh] ))
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
   fi
 }
 
@@ -73,11 +54,7 @@ directory_name(){
 }
 
 export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
 }
